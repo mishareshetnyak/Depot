2d4465773fd8349eaae1ab360303526fe7a45942
misha reshetnyak <mihareshetnyak@gmail.com>
2014-11-25 16:44:36 +0200

    fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ActiveRecord::Base
     Product.order(:updated_at).last
   end
   before_destroy :ensure_not_referenced_by_any_line_item
+  private
   def ensure_not_referenced_by_any_line_item
     if line_items.empty?
       return true
@@ -12,5 +13,4 @@ class Product < ActiveRecord::Base
       return false
     end
   end
-end
 end
